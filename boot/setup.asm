228f48f7964ab0e534c37ffdb326a058698f4f15
lambdafate <lambdafate@163.com>
2020-03-21 11:59:18 +0800

    jmp to protected mode

--- boot/setup.asm
+++ boot/setup.asm
@@ -1,8 +1,75 @@
 ;  file: /boot/setup.asm
 ;   
 
-    CurrCS  equ 0x07e0
-    
+    CurrCS          equ     0x07e0
+
+    GDTlIMIT        equ     39
+    GDTBaseR        equ     0x07d0  
+    GDTBaseP        equ     0x00007d00
+
+    ; selectors
+    CodeSelector    equ     0x08
+    ScreenSelector  equ     0x10
+    DataSelector    equ     0x18
+    StackSelector   equ     0x20
+
+    jmp begin_setup
+
+; some api, don't place those after '[bits 32]'
+; return: eax-> memory size
+read_memory_size:
+    push es
+
+    mov ax, 0x07c0
+    mov es, ax
+    mov di, 0x0000
+    mov ecx, 20
+    mov ebx, 0
+    mov ebp, 0
+_loopread:
+    mov eax, 0xe820             ; function number
+    mov edx, 0x534d4150
+    int 0x15
+    jc _readerror
+    mov eax, [es:di]
+    add eax, [es:di+8]
+    cmp ebp, eax
+    ja _isreadfinished
+    mov ebp, eax
+_isreadfinished:
+    cmp ebx, 0
+    je _readsucces
+    jmp _loopread
+
+_readsucces:   
+    mov eax, ebp
+    jmp short _readfinish
+_readerror:
+    mov eax, 0
+_readfinish:
+    pop es
+    ret
+
+; es:bp -> string
+; cx    -> string size
+; bl    -> color
+print:
+    ; read lightpoint position, then display message
+    mov bh, 0x00
+    mov al, 0x01
+    mov ah, 0x13
+    int 0x10
+    ret
+
+readpoint:
+    mov bh, 0x00
+    mov ah, 0x03
+    int 0x10
+    ret
+
+
+
+begin_setup:
     mov ax, 0x07c0
     mov ds, ax
 
@@ -53,63 +120,71 @@ _loopwrite:
     mov cx, 30
     mov bl, 7
     call print
+
+    ; loading kernel to 0x10000(64kb)
+
+
+    ; prepare for into protected mode
+    mov ax, GDTBaseR
+    mov ds, ax
+    ; 装载GDT
+    mov dword [ds:0x00], 0x00000000
+    mov dword [ds:0x04], 0x00000000
+
+    mov dword [ds:0x08], 0x7e0003ff         ; 代码段
+    mov dword [ds:0x0c], 0x00409800         
+
+    mov dword [ds:0x10], 0x80007fff         ; screen display
+    mov dword [ds:0x14], 0x0040920b
+
+    mov dword [ds:0x18], 0x0000ffff         ; 数据段
+    mov dword [ds:0x1c], 0x00cf9200
+
+    mov dword [ds:0x20], 0x00000000         ; 堆栈段
+    mov dword [ds:0x24], 0x00409600
+
+    ; 填写GDTR
+    lgdt [cs:gdtinfo]   ; cs=0x07e0
+
+    ; 打开A20
+    in al, 0x92
+    or al, 000000_1_0B
+    out 0x92, al
+
+    ; 关闭中断
+    cli
+
+    ; 修改PE位
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp dword 0x08:flush
+
+    [bits 32]
+flush:
+    mov ax, DataSelector
+    mov ds, ax
+
+    mov ax, StackSelector
+    mov ss, ax
+    mov esp, 0x7c00
+
+    mov ax, ScreenSelector
+    mov es, ax
+
+    push esp
+
+    mov ebx, (20*80+40)*2
+    mov byte [es:ebx], 'Y'
+    mov byte [es:ebx+1], 4 
     jmp $
 
 
-; return: eax-> memory size
-read_memory_size:
-    push es
-
-    mov ax, 0x07c0
-    mov es, ax
-    mov di, 0x0000
-    mov ecx, 20
-    mov ebx, 0
-    mov ebp, 0
-_loopread:
-    mov eax, 0xe820             ; function number
-    mov edx, 0x534d4150
-    int 0x15
-    jc _readerror
-    mov eax, [es:di]
-    add eax, [es:di+8]
-    cmp ebp, eax
-    ja _isreadfinished
-    mov ebp, eax
-_isreadfinished:
-    cmp ebx, 0
-    je _readsucces
-    jmp _loopread
-
-_readsucces:   
-    mov eax, ebp
-    jmp short _readfinish
-_readerror:
-    mov eax, 0
-_readfinish:
-    pop es
-    ret
-
-
-; es:bp -> string
-; cx    -> string size
-; bl    -> color
-print:
-    ; read lightpoint position, then display message
-    mov bh, 0x00
-    mov al, 0x01
-    mov ah, 0x13
-    int 0x10
-    ret
-
-readpoint:
-    mov bh, 0x00
-    mov ah, 0x03
-    int 0x10
-    ret
-
-
-
+; gdt data
+gdtinfo:
+    dw  GDTlIMIT
+    dd  GDTBaseP
 
 stepinfo:
     db "[/boot/setup          ]: "                      ; length 25
