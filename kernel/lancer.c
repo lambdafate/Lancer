2b49427326ad79002a8a2b5bbaced4209b485541
lambdafate <lambdafate@163.com>
2020-03-27 22:37:50 +0800

    :boom: update kernel!

--- kernel/lancer.c
+++ kernel/lancer.c
@@ -1,5 +1,39 @@
+#include "print.h"
 
+void test_func_put_char();
 int main(){
 
+	test_func_put_char();
+
+	char* str = "Hello, fuck, we must follow 'cdecl', if you call a fuction, for example call put_char here, you must deal with args on stack";
+	put_str(str);
+
 	while(1){}
+}
+
+
+void test_func_put_char(){
+	// test put_char
+	put_char('f'); put_char('u'); put_char('c'); put_char('k');
+
+	// test backspace
+	put_char(8);
+
+	// test '\r'
+	put_char('\r'); put_char('r');
+
+	// test '\n'
+	put_char('\n'); put_char('n'); put_char('n');
+
+	int num = 80;
+	while(num--){
+		put_char('n');
+	}
+
+	// test rollscreen
+	num = 20;
+	while(num--){
+		put_char('\n');
+	}
+	put_char('n'); put_char('e'); put_char('w');
 }
--- run.sh
+++ run.sh
@@ -3,11 +3,15 @@
 nasm ./boot/boot.asm -o BOOT && 					# load boot to sector1
 dd bs=512 if=BOOT of=Image count=1 conv=notrunc && 
 rm ./BOOT &&
+
 nasm ./boot/setup.asm -o SETUP &&					# load setup to sector2~sector5
 dd bs=512 if=SETUP of=Image count=4 seek=1 conv=notrunc &&
 rm ./SETUP &&
-gcc -m32 -c ./kernel/lancer.c -o lancer.o &&				# load kernel to sector6~
-ld -Ttext 0xc0010000 -e main -m elf_i386 -o LANCER lancer.o &&
+
+
+gcc -m32 -I lib/kernel/ -c ./kernel/lancer.c -o lancer.o &&				# load kernel to sector6~
+nasm -f elf ./lib/kernel/print.asm -o print.o  &&
+ld -Ttext 0xc0010000 -e main -m elf_i386 -o LANCER lancer.o print.o &&
 dd bs=512 if=LANCER of=Image count=200 seek=5 conv=notrunc &&
-rm lancer.o LANCER &&
+rm lancer.o print.o LANCER &&
 bochs -q
