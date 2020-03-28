702f5c5216a751e95e50ea3a1f04cc43c5e8180a
lambdafate <lambdafate@163.com>
2020-03-28 13:29:11 +0800

    :sparkles: jmp into kernel!

--- kernel/lancer.c
+++ kernel/lancer.c
@@ -1,15 +1,25 @@
 #include "print.h"
 
 void test_func_put_char();
+void test_func_put_char();
+void test_func_put_hex();
+void test_func_put_int();
+void welcome();
+
 int main(){
 
-	test_func_put_char();
+	welcome();
 
-	char* str = "Hello, fuck, we must follow 'cdecl', if you call a fuction, for example call put_char here, you must deal with args on stack";
-	put_str(str);
-
+	//test_func_put_hex();
+	//test_func_put_int();
 	while(1){}
 }
+
+void welcome(){
+	put_str("[/kernel/lancer       ]: ");
+	put_str("welcome to lancer!\n");
+}
+
 
 
 void test_func_put_char(){
@@ -37,3 +47,38 @@ void test_func_put_char(){
 	}
 	put_char('n'); put_char('e'); put_char('w');
 }
+
+
+void test_fuc_put_str(){
+	int line = 24;
+	while(line--){}
+	char* str = "Hello, fuck, we must follow 'cdecl', if you call a fuction, for example call put_char here, you must deal with args on stack";
+	put_str(str);
+}
+
+void test_func_put_hex(){
+	put_str("\n hex1: \0");
+	put_hex(0x12345678);
+	put_str(" hex2: \0");
+	put_hex(0x0000);
+	put_str(" hex3: \0");
+	put_hex(0xffffffff);
+	put_str(" hex4: \0");
+	put_hex(0xf00e0aca);
+}
+
+void test_func_put_int(){
+	put_str("\n int1: \0");
+	put_int(123456);
+	put_str(" int2: \0");
+	put_int(0xffffffff);
+	put_str(" int3: \0");
+	put_int(0);
+	put_str(" int4: \0");
+	put_int(-1);
+	
+}
+
+
+
+
