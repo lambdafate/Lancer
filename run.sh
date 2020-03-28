94fc1663522bf3ae07b322129fac0738c8d9692b
lambdafate <lambdafate@163.com>
2020-03-28 13:31:11 +0800

    :wrench: update

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ rm ./SETUP &&
 
 gcc -m32 -I lib/kernel/ -c ./kernel/lancer.c -o lancer.o &&				# load kernel to sector6~
 nasm -f elf ./lib/kernel/print.asm -o print.o  &&
-ld -Ttext 0xc0010000 -e main -m elf_i386 -o LANCER lancer.o print.o &&
+ld -Ttext 0xc0010000 -e main -m elf_i386 -o LANCER *.o  &&
 dd bs=512 if=LANCER of=Image count=200 seek=5 conv=notrunc &&
-rm lancer.o print.o LANCER &&
+rm *.o &&
 bochs -q
