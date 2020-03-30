5d6b65f9be4109d019e47897adf05d918e8ddcb3
lambdafate <lambdafate@163.com>
2020-03-30 23:02:02 +0800

    :fire: update

--- run.sh
+++ run.sh
@@ -8,10 +8,12 @@ nasm ./boot/setup.asm -o SETUP &&					# load setup to sector2~sector5
 dd bs=512 if=SETUP of=Image count=4 seek=1 conv=notrunc &&
 rm ./SETUP &&
 
-
-gcc -m32 -I lib/kernel/ -c ./kernel/lancer.c -o lancer.o &&				# load kernel to sector6~
-nasm -f elf ./lib/kernel/print.asm -o print.o  &&
-ld -Ttext 0xc0010000 -e main -m elf_i386 -o LANCER *.o  &&
+# load kernel to sector6~
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -fno-stack-protector  -c ./kernel/lancer.c -o build/lancer.o &&				
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/  -fno-stack-protector  -c ./kernel/interrupt.c -o build/interrupt_c.o &&
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/   -fno-stack-protector  -c ./kernel/init.c -o build/init.o  &&
+nasm -f elf ./lib/kernel/print.asm -o build/print.o  &&
+nasm -f elf ./kernel/interrupt.asm -o build/interrupt_asm.o &&
+ld -Ttext 0xc0010000 -e main -m elf_i386 -o LANCER build/*.o  &&
 dd bs=512 if=LANCER of=Image count=200 seek=5 conv=notrunc &&
-rm *.o &&
 bochs -q
