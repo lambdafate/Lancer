24b36e7c9a8acd7c8a03ded3f059e9d924454878
lambdafate <lambdafate@163.com>
2020-03-30 22:58:44 +0800

    :smile: operate port

---
+++ lib/kernel/io.h
@@ -0,0 +1,24 @@
+#ifndef _LIB_KERNEL_IO_H
+#define _LIB_KERNEL_IO_H
+#include "stdint.h"
+
+static inline void outb(uint16_t port, uint8_t data){
+    asm volatile ("outb  %b0, %w1": : "a"(data), "Nd"(port));
+} 
+
+static inline void outsw(uint16_t port, const void* addr, uint32_t count){
+    asm volatile ("cld; rep outsw": "+S"(addr), "+c"(count): "d"(port));
+}
+
+static inline uint8_t inb(uint16_t port){
+    uint8_t data;
+    asm volatile ("inb %w1, %b0": "=a"(data): "Nd"(port));
+    return data;
+}
+
+static inline void insw(uint16_t port, void* addr, uint32_t count){
+    asm volatile ("cld; rep insw": "+D"(addr), "+c"(count): "d"(port): "memory");
+}
+
+
+#endif
