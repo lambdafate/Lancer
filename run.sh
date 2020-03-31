394b2a4be21c16b18bdd409c6d40535de946db46
lambdafate <lambdafate@163.com>
2020-03-31 11:30:04 +0800

    :smile: update

--- run.sh
+++ run.sh
@@ -9,9 +9,10 @@ dd bs=512 if=SETUP of=Image count=4 seek=1 conv=notrunc &&
 rm ./SETUP &&
 
 # load kernel to sector6~
-gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -fno-stack-protector  -c ./kernel/lancer.c -o build/lancer.o &&				
-gcc -m32 -I lib/kernel/ -I lib/ -I kernel/  -fno-stack-protector  -c ./kernel/interrupt.c -o build/interrupt_c.o &&
-gcc -m32 -I lib/kernel/ -I lib/ -I kernel/   -fno-stack-protector  -c ./kernel/init.c -o build/init.o  &&
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -I device/  -fno-stack-protector  -c ./kernel/lancer.c -o build/lancer.o &&				
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -I device/  -fno-stack-protector  -c ./kernel/interrupt.c -o build/interrupt_c.o &&
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -I device/  -fno-stack-protector  -c ./kernel/init.c -o build/init.o  &&
+gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -I device/  -fno-stack-protector  -c ./device/timer.c -o build/timer.o  &&
 nasm -f elf ./lib/kernel/print.asm -o build/print.o  &&
 nasm -f elf ./kernel/interrupt.asm -o build/interrupt_asm.o &&
 ld -Ttext 0xc0010000 -e main -m elf_i386 -o LANCER build/*.o  &&
