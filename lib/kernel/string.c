c29dac1252772a82c6f2dc1087be8689a9681fa3
lambdafate <lambdafate@163.com>
2020-04-09 13:29:30 +0800

    :rocket: add string op func

--- lib/kernel/string.c
+++ lib/kernel/string.c
@@ -1,10 +1,27 @@
 #include "stdint.h"
 
 
-void memory_set(uint8_t* _destination, uint32_t size, uint8_t value){
+void memory_set(uint8_t* dest, uint32_t size, uint8_t value){
     uint32_t index = 0;
-    uint8_t* destination = _destination;
+    uint8_t* destination = dest;
     while(index++ < size){
         *destination++ = value;
     }
+}
+
+uint32_t strlen(uint8_t *str){
+    uint32_t len = 0;
+    while(*str++){
+        len++;
+    }
+    return len;
+}
+
+uint8_t* strcpy(uint8_t *dest, uint8_t *src){
+    uint8_t *res = dest;
+    while(*src){
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+    return res;
 }
--- lib/kernel/string.h
+++ lib/kernel/string.h
@@ -1,6 +1,9 @@
 #ifndef _LIB_KERNEL_STRING_H
 #include "stdint.h"
 
-void memory_set(uint8_t* _destination, uint32_t size, uint8_t value);
+
+void memory_set(uint8_t* dest, uint32_t size, uint8_t value);
+uint32_t strlen(uint8_t *str);
+uint8_t* strcpy(uint8_t *dest, uint8_t *src);
 
 #endif
