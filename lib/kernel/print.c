a875d37a1b247d0fad672c25a0425b20c7dd57a6
lambdafate <lambdafate@163.com>
2020-04-12 19:17:28 +0800

    :sparkles: add printk, printf, vsprintf

--- lib/kernel/print.c
+++ lib/kernel/print.c
@@ -1,17 +1,22 @@
 #include "stdint.h"
 #include "print.h"
 #include "string.h"
+#include "syscall.h"
 #include "debug.h"
 
 int32_t vsprintf(int8_t *buffer, int8_t *fmt, int32_t *args);
 
+// use in user mode(ring3).
 void printf(int8_t *fmt, ...){
     int8_t buffer[256];
-
+    vsprintf(buffer, fmt, (int32_t*)((int32_t)(&fmt)+4));
+    sys_write(buffer);
 }
 
+
+// use in kernel mode(ring0)
 void printk(int8_t *fmt, ...){
-    int8_t buffer[256];
+    uint8_t buffer[256];
     vsprintf(buffer, fmt, (int32_t*)((uint32_t)(&fmt)+4));
     put_str(buffer);
 }
@@ -19,7 +24,7 @@ void printk(int8_t *fmt, ...){
 
 int32_t vsprintf(int8_t *buffer, int8_t *fmt, int32_t *args){
     int8_t *save = buffer;
-    int8_t *work;
+    int8_t *work = NULL;
     while (*fmt){
         if(*fmt != '%'){
             *save++ = *fmt++;
@@ -28,25 +33,23 @@ int32_t vsprintf(int8_t *buffer, int8_t *fmt, int32_t *args){
         fmt++;
         switch (*fmt){
             case 's':
-                work = (int8_t*)(*args);
+                work = (int8_t*)(*args++);
                 while(*work){
                     *save++ = *work++;
                 }
-                
-            case 'c':
-                *save++ = (int8_t)(*args);
-                
-            default:
                 fmt++;
-                args++;
+                break;
+            case 'c':
+                *save++ = (int8_t)(*args++);
+                fmt++;
+                break;
+            default:
+                ASSERT(0);
                 break;
         }
     }
-    // ASSERT(save == buffer);
+    
     *save = '\0';
-    put_hex(save);
-    put_char('\n');
-    put_hex(buffer);
     return save-buffer;
 }
 
