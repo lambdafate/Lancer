30bded33c99bab2e2c4119e8c28e921e54867635
lambdafate <lambdafate@163.com>
2020-04-25 13:44:24 +0800

    :rocket: update

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -29,61 +29,6 @@ uint32_t sys_get_ticks(){
 	return ticks;
 }
 
-void print_mbr(struct part *p){
-    printk("boot: %x  part-type: %x  offset: %x   sum-sectors: %x\n", p->boot, p->part_type, p->sectors_offset, p->sectors_count);
-    printk("start-head: %x   start-sector: %x  start-cylinder: %x\n", p->start_head, p->start_sector, p->start_cylinder);
-    printk("end-head  : %x   end-sector  : %x  end-cylinder  : %x\n", p->end_head, p->end_sector, p->end_cylinder);
-}
-
-
-void test(){
-    uint8_t *buffer = (uint8_t*)(0x0000);
-    if(sata_read(0, 1, buffer) == -1){
-        printk("Read error!\n");
-        return;
-    }
-    ASSERT(*(buffer+510) == 0x55);
-    ASSERT(*(buffer+511) == 0xaa);
-
-    memcpy(mbrparts, buffer+446, 64);
-    uint32_t logical_index = 0;
-
-    for(uint32_t i=0; i < 4; i++){
-        if(mbrparts[i].part_type == 0x00){
-            continue;
-        }
-        if(mbrparts[i].part_type != 0x05){
-            printk("\n    ******     %x: master partition    *******\n", i);
-            print_mbr(&mbrparts[i]);
-            continue;
-        }
-        // it is a extend partition
-        printk("\n    ******     %x: extend partition   *******\n", i);
-
-        ASSERT(sata_read(mbrparts[i].sectors_offset, 1, buffer) != -1);
-        memcpy(&logicalparts[logical_index], buffer+446, 32);
-        print_mbr(&logicalparts[logical_index++]);
-
-        ASSERT(*(buffer+510) == 0x55);
-        ASSERT(*(buffer+511) == 0xaa);
-
-        while(logical_index < 8 && logicalparts[logical_index].part_type != 0){
-            uint32_t begin_sector = mbrparts[i].sectors_offset + logicalparts[logical_index++].sectors_offset;
-            
-            sata_read(begin_sector, 1, buffer);
-            memcpy(&logicalparts[logical_index], buffer+446, 32);
-            
-            ASSERT(*(buffer+510) == 0x55);
-            ASSERT(*(buffer+511) == 0xaa);
-
-            printk("\n    ******     extend partition    *******\n");
-            print_mbr(&logicalparts[logical_index++]);
-        }    
-
-    }
-}
-
-
 
 void handler_syscall(){
     uint32_t syscall_numer = 0;
@@ -101,7 +46,6 @@ void handler_syscall(){
             put_str((int8_t*)arg1);
             return;
         case SYSCALL_NUMBER_HD_IDENTIFY:
-            test();
             return;
         default:
             break;
