a455ed1bfd8cb5afb4dfda5a6560637063a98d88
lambdafate <lambdafate@163.com>
2020-04-26 19:19:43 +0800

    :sparkles: update file system.

--- fs/fs.c
+++ fs/fs.c
@@ -11,41 +11,30 @@
 #include "dir.h"
 #include "inode.h"
 #include "hd.h"
+#include "malloc.h"
 #include "string.h"
 #include "print.h"
 #include "debug.h"
 
+void *get_super_block(struct partition *part);
 
-void fs_test(){
-    // test master0 partition
-    uint8_t *buffer = (uint8_t*)(0xc0000000);
-    sata_read(sata0_master.master_parts[0].start_sector+1, 1, buffer);
-
-    struct super_block *sb = (struct super_block*)(buffer);
-    ASSERT(sb->magic = FS_MAGIC);
-    ASSERT(sb->root_inode == 0);
-
-    sata_read(sb->inode_table_lba, 1, buffer);
-    struct inode *inodes = (struct inode*)(buffer);
-    ASSERT(inodes->i_no == 0);
-    ASSERT(inodes->i_size == sizeof(struct dir_entry)*2);
-
-    sata_read(inodes->i_blocks[0], 1, buffer);
-    struct dir_entry *des = (struct dir_entry*)(buffer);
-    printk("/\n");
-    for(uint32_t i=0; i < 2; i++){
-        printk("name: %s   i_no: %x    file-type: %x\n", des->name, des->i_no, des->file_type);
-        des++;
-    }
-
-    while(1){
-
-    }
-}
 
 void fs_init(){
+    
+    // check every partition, for building file system
+    check_fs();
+    // init fd_table, used in proc.
+    init_fd_table();
+
+    // malloc space for block_bitmap, inode_bitmap, inode_table
+    init_fs_meta();
+
+}
+
+void check_fs(){
     struct super_block *sb = (struct super_block*)(0xc0000000);
     
+    // we only use one partition now.
     for(uint32_t i = 0; i < 1; i++){
         sata_read(sata0_master.master_parts[i].start_sector+1, 1, sb);
         if(sb->magic != FS_MAGIC){
@@ -60,9 +49,39 @@ void fs_init(){
         }
     }
     */
-
-   fs_test();
 }
+
+void init_fd_table(){
+    for(uint32_t i = 0; i < FD_TABLE_MAX_SIZE; i++){
+        fd_table[i].f_mode = 0;
+        fd_table[i].f_pos = 0;
+        fd_table[i].f_inode = NULL;
+    }
+}
+
+void init_fs_meta(){
+    // init super block
+    sb = (struct super_block*)get_super_block(&sata0_master.master_parts[0]);
+    printk("\nsuper-block: %x\n", sb);
+
+    block_bitmap.size = sb->block_bitmap_sectors * SECTOR_SIZE * 8;
+    block_bitmap.bits = (uint8_t*)vmalloc(sb->block_bitmap_sectors * SECTOR_SIZE);
+    printk("block-bitmap sectors: %x  block-bitmap.bits: %x\n", sb->block_bitmap_sectors, block_bitmap.bits);
+    sata_read(sb->block_bitmap_lba, sb->block_bitmap_sectors, block_bitmap.bits);
+
+    // init inode_bitmap
+    inode_bitmap.size = sb->inode_bitmap_sectors * SECTOR_SIZE * 8;
+    inode_bitmap.bits = (uint8_t*)vmalloc(sb->inode_bitmap_sectors * SECTOR_SIZE);
+    printk("inode-bitmap sectors: %x  inode-bitmap.bits: %x\n", sb->inode_bitmap_sectors, inode_bitmap.bits);
+    sata_read(sb->inode_bitmap_lba, sb->inode_bitmap_sectors, inode_bitmap.bits);
+
+    // init inode_table
+    inode_table = (struct inode*)vmalloc(sb->inode_table_sectors * SECTOR_SIZE);
+    printk("inode-table sectors : %x  inode-table: %x\n", sb->inode_table_sectors, inode_table);
+    sata_read(sb->inode_table_lba, sb->inode_table_sectors, inode_table);
+
+}
+
 
 
 void format_part(struct partition *part){
@@ -141,6 +160,40 @@ void format_part(struct partition *part){
 }
 
 
+void *get_super_block(struct partition *part){
+    void *buffer = (void*)vmalloc(512);
+    sata_read(part->start_sector+1, 1, buffer);
+    return buffer;
+}
 
 
+void fs_test(){
+    // test master0 partition
+    uint8_t *buffer = (uint8_t*)(0xc0000000);
+    sata_read(sata0_master.master_parts[0].start_sector+1, 1, buffer);
 
+    struct super_block *sb = (struct super_block*)(buffer);
+    ASSERT(sb->magic = FS_MAGIC);
+    ASSERT(sb->root_inode == 0);
+
+    printk("block_bitmap sectors: %x\n", sb->block_bitmap_sectors);
+    printk("inode_bitmap sectors: %x\n", sb->inode_bitmap_sectors);
+    printk("inode_table  sectors: %x\n", sb->inode_table_sectors);
+
+    sata_read(sb->inode_table_lba, 1, buffer);
+    struct inode *inodes = (struct inode*)(buffer);
+    ASSERT(inodes->i_no == 0);
+    ASSERT(inodes->i_size == sizeof(struct dir_entry)*2);
+
+    sata_read(inodes->i_blocks[0], 1, buffer);
+    struct dir_entry *des = (struct dir_entry*)(buffer);
+    printk("/\n");
+    for(uint32_t i=0; i < 2; i++){
+        printk("name: %s   i_no: %x    file-type: %x\n", des->name, des->i_no, des->file_type);
+        des++;
+    }
+
+    while(1){
+
+    }
+}
--- fs/fs.h
+++ fs/fs.h
@@ -1,6 +1,8 @@
 #ifndef _FS_FS_H
 #define _FS_FS_H
 #include "stdint.h"
+#include "inode.h"
+#include "bitmap.h"
 
 // per part's max files
 #define PART_MAX_FILES_NUM              4096
@@ -10,14 +12,27 @@
 
 #define FS_MAGIC                        0x12345678
 
-
 enum file_types{
     FT_REGULAR, FT_DIRECTORY, FT_UNKNOWN
 };
 
 
-void fs_init();
+#define FD_TABLE_MAX_SIZE               (240)
+struct file_descriptor{
+    uint32_t f_mode;
+    uint32_t f_pos;
+    struct inode * f_inode;
+};
 
+struct file_descriptor fd_table[FD_TABLE_MAX_SIZE];
+
+struct super_block *sb;
+BITMAP block_bitmap;
+BITMAP inode_bitmap;
+struct inode *inode_table;
+
+
+void fs_init();
 
 
 #endif
